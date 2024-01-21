a2c131df5519ba0513f55c66aef5aa3e991142a3
Deepanshuisjod <deepanshudb1@gmail.com.noreply.github.com>
2024-01-21 16:44:18 +0530

    Modified server.py file
    recieved the request as header and question from the client side i.e. client.py and also created a dns_answer_instance and pass the values to attribtes in dataclass(DNSanswersection) of DNSmessage.py

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 import socket
-
-SERVER , PORT = 'localhost' , 2053
+from DNSmessage import DNSanswerSection
+import ipaddress
+SERVER, PORT = 'localhost', 2053
 
 if __name__ == '__main__':
-    server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # Created a UDP socket for IPv4
-    server.bind((SERVER,PORT))
-    data, client_address = server.recvfrom(1024) 
-    request = data.decode('utf-8')
-    print(f"Connection from client : {data}")
-    response = "Hi from the server side !!"
-    server.sendto(response.encode('utf-8'), client_address)
-    
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Created a UDP socket for IPv4
+    server.bind((SERVER, PORT))
+
+    while True:
+        data, client_address = server.recvfrom(1024)
+        print(f"Connection from client: {client_address[0]}")
+        print(f"Request (binary): {data}")
+        int_client_address = int(ipaddress.IPv4Address(client_address[0]))
+        dns_answer_instance = DNSanswerSection(TTL=60, RDATA=int_client_address, RDLENGTH=4)
+        server.sendto(data + dns_answer_instance.get_values(), client_address)
