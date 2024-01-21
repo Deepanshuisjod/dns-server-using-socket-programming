d74ace8ca6b9b1e8afcee29619ec2891cfec36ff
Deepanshuisjod <deepanshudb1@gmail.com.noreply.github.com>
2024-01-21 16:42:20 +0530

    Modified clien.py file
    created the dns_header_instance and pass the values to attributes in the DNSmessage.py file . Also created dns_question_instance and pass the values to attributes in the DNSmessage.py file

--- client.py
+++ client.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 import socket
+import struct
+from DNSmessage import DNSheader , DNSquestionSection
 
-SERVER_IP , SERVER_PORT = 'localhost' , 2053
+SERVER_IP, SERVER_PORT = 'localhost', 2053
 
-client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-client.connect((SERVER_IP,SERVER_PORT))
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-request = "Hii from client"
-client.sendall(request.encode('utf-8'))
 
-response = client.recv(4096).decode('utf-8')
+dns_header_instance = DNSheader(
+    id = 1234, qr = 1, opcode = 0, AA = 0, TC = 0,
+    RD = 0, RA = 0, Z = 0, RCODE = 0,
+    QDCOUNT = 0, ANCOUNT = 0, NSCOUNT = 0, ARCOUNT = 0
+)
+dns_question_instance = DNSquestionSection("google.com",1,1)
+
+client.sendto(dns_header_instance.get_values() + dns_question_instance.domain_name() + dns_question_instance.type_class_(),(SERVER_IP,SERVER_PORT))
+response = client.recvfrom(1024)
 print(response)
-
 client.close()
