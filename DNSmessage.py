ddb0f32da681cdd938048982b060b50e10b04b6d
Deepanshuisjod <deepanshudb1@gmail.com.noreply.github.com>
2024-01-21 16:40:15 +0530

    Modified DNSmessage.py file
    added the struct libray to get the attributes in bytes and return all the packed values to client and server files for the requests and responses respectively. Done this for header question and answer section dataclasess

--- DNSmessage.py
+++ DNSmessage.py
@@ -15,7 +15,7 @@ into 5 sections (some of which are empty in certain cases) shown below:
     +---------------------+ '''
 
 from dataclasses import dataclass
-
+import struct
 @dataclass
 class DNSheader :
     id : int    # A random ID assignet to Query packets . A random ID assigned to query packets. Response packets must reply with the same ID. 
@@ -32,18 +32,32 @@ class DNSheader :
     NSCOUNT : int   # NSCOUNT is Authority record count . Number of records in the Authority section. 
     ARCOUNT : int   # ARCOUNT is Additional record count . Number of records in the additional section.
     
+    def get_values(self) -> bytes:
+        flags = (self.qr << 15) | (self.opcode << 11) | (self.AA << 10) | (self.TC << 9) | (self.RD << 8) | (self.RA << 7) | (self.Z << 4) | (self.RCODE)
+        return struct.pack(">HHHHHH",self.id,flags,self.QDCOUNT,self.ANCOUNT,self.NSCOUNT,self.ARCOUNT)
 @dataclass
 class DNSquestionSection:
     Name : str      # Domain Name which is the sequence of label
     type : int      # type is the type of record A(address) - 1, NS(name server) - 2, MX(Mail Exchange) - 3, CNAME(Cannonical/Alias Name of server) - 5 
     class_ : int    # Usually set to 1 for internet (IN)
 
+    def domain_name(self):
+        labels = self.Name.split('.')
+        encoded_labels = [bytes([len(label)]) + label.encode('utf-8') for label in labels]
+        encoded_name = b''.join(encoded_labels) + b'\x00'
+        return encoded_name
+    
+    def type_class_(self) -> bytes:
+        type_n_class = struct.pack(">HH",self.type,self.class_)
+        return type_n_class
 @dataclass
 class DNSanswerSection:
-    NTC : DNSquestionSection
     TTL : int       # The duration in seconds a record can be cached before requerying.
     RDLENGTH : int  # Length of the RDATA field in bytes.
     RDATA: int      # IP address
+
+    def get_values(self):
+        return struct.pack(">IIH",self.TTL,self.RDATA,self.RDLENGTH)
 
 @dataclass
 class DNSauthoritativeSection:
