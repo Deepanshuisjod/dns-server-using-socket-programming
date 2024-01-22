9aa0d3759612991d460f80734301e0f9f7766a2c
Deepanshuisjod <deepanshudb1@gmail.com>
2024-01-22 19:07:26 +0530

    modified client.py
    modified the whole file

--- client.py
+++ client.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python3
 import socket
-import struct
-from DNSmessage import DNSheader , DNSquestionSection
+
 
 SERVER_IP, SERVER_PORT = 'localhost', 2053
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Client side socket
 
+message = "Client Requests IP(IPv4)"
+client.sendto(message.encode('utf-8'),(SERVER_IP,SERVER_PORT))
 
-dns_header_instance = DNSheader(
-    id = 1234, qr = 1, opcode = 0, AA = 0, TC = 0,
-    RD = 0, RA = 0, Z = 0, RCODE = 0,
-    QDCOUNT = 0, ANCOUNT = 0, NSCOUNT = 0, ARCOUNT = 0
-)
-dns_question_instance = DNSquestionSection("google.com",1,1)
-
-client.sendto(dns_header_instance.get_values() + dns_question_instance.domain_name() + dns_question_instance.type_class_(),(SERVER_IP,SERVER_PORT))
 response_ = client.recvfrom(1024)
 print(response_)
 client.close()
